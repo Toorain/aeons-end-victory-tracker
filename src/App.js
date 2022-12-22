65fafe198d1d8f480a311429107d859080ce1485
Maxence WILMES <maxencewilmes@gmail.com>
2022-12-23 00:25:23 +0100

    Base Layout

--- src/App.js
+++ src/App.js
@@ -15,27 +15,35 @@ function App() {
     {
       setName: t('basicSet'),
       mageList: ['Adelhelm', 'Brama', 'Jian', 'Kadir', 'Lash', 'Mist', 'Phaedraxa'],
-      nemesisList: [t('rageBorne'), t('princeOfGluttons'), t('crooked'), t('carapace')]
+      nemesisList: [
+        {name: 'rageBorne', src: 'rageBorne'},
+        {name: 'princeOfGluttons', src: 'princeOfGluttons'},
+        {name: 'crookedMask', src: 'crookedMask'},
+        {name: 'carapaceQueen', src: 'carapaceQueen'}
+      ]
     },
     {
       setName: t('warEternal'),
       mageList: ['Adelhelm', 'Brama', 'Jian', 'Kadir', 'Lash', 'Mist', 'Phaedraxa'],
-      nemesisList: [t('rageBorne'), t('princeOfGluttons'), t('crooked'), t('carapace')]
+      nemesisList: [
+        {name: 'rageBorne', src: 'rageBorne'},
+        {name: 'princeOfGluttons', src: 'princeOfGluttons'},
+        {name: 'crookedMask', src: 'crookedMask'},
+        {name: 'carapaceQueen', src: 'carapaceQueen'}
+      ]
     },
   ]
 
   return (
     <div className="App">
-      <header className="App-header">
-        <ButtonGroup variant="contained" aria-label="contained primary button group">
-          <div>
-            {Object.keys(languages).map((language) => (
-              <Button type={"submit"} key={language} onClick={() => i18n.changeLanguage(language)} disabled={i18n.resolvedLanguage === language}>{languages[language].nativeName}</Button>
-            )) }
-          </div>
-        </ButtonGroup>
-        <Expansion sets={sets} />
-      </header>
+      <ButtonGroup variant="contained" aria-label="contained primary button group">
+        <div>
+          {Object.keys(languages).map((language) => (
+            <Button type={"submit"} key={language} onClick={() => i18n.changeLanguage(language)} disabled={i18n.resolvedLanguage === language}>{languages[language].nativeName}</Button>
+          )) }
+        </div>
+      </ButtonGroup>
+      <Expansion sets={sets} />
     </div>
   );
 }
--- src/components/Expansion/Expansion.js
+++ src/components/Expansion/Expansion.js
@@ -1,17 +1,13 @@
 import Nemesis from "../Nemesis/Nemesis";
-import {Box, Grid, Paper, styled} from "@mui/material";
+import {Box, Grid, Paper, styled, Typography} from "@mui/material";
 import Difficulty from "../Difficulty/Difficulty";
 import {useTranslation} from "react-i18next";
 
-const Item = styled(Paper)(({ theme }) => ({
+const Item = styled(Paper)(() => ({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
   textAlign: 'center',
-  color: theme.palette.text.secondary,
   height: '100%',
 }));
 
@@ -28,23 +24,32 @@ function Expansion(props) {
         <div>
         {Object.values(props.sets).map((set) => (
           <Grid container spacing={1}>
-            <Grid item xs={4}>
-              <Item>
-                <Nemesis nemesisName={'rageBorne'} imageSrc={'rageBorne'} />
-              </Item>
+            <Grid item xs={12}>
+              <Typography>{set.setName}</Typography>
             </Grid>
-            <Grid container xs={8}>
-              {Object.values(difficulties).map((difficulty) => (
-                <Grid item xs={4}>
-                  <Item sx={{
-                    display: 'flex',
-                    flexDirection: 'column'
-                  }}>
-                    <Difficulty level={difficulty} />
-                  </Item>
+            {Object.values(set.nemesisList).map((nemesis) => (
+              <>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <Item>
+                      <Nemesis nemesisName={nemesis.name} imageSrc={nemesis.src} />
+                    </Item>
+                  </Grid>
+                  <Grid container xs={8}>
+                    {Object.values(difficulties).map((difficulty) => (
+                    <Grid item xs={4}>
+                      <Item sx={{
+                          display: 'flex',
+                          flexDirection: 'column'
+                        }}>
+                      <Difficulty level={difficulty} />
+                      </Item>
+                    </Grid>
+                )) }
                 </Grid>
-              )) }
-            </Grid>
+              </Grid>
+              </>
+            ))}
           </Grid>
         )) }
         </div>
