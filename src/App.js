fc9cca56355afee02d0d55cb23399a5580d03512
Maxence WILMES <maxencewilmes@gmail.com>
2022-12-22 18:04:06 +0100

    Commit before MUI React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {useTranslation} from "react-i18next";
 
+const languages = {
+  fr: {nativeName: 'Français'},
+  en: {nativeName: 'English'}
+}
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          {Object.keys(languages).map((language) => (
+            <button type={"submit"} key={language} onClick={() => i18n.changeLanguage(language)} disabled={i18n.resolvedLanguage === language}>{languages[language].nativeName}</button>
+            )) }
+        </div>
+        {t('rageBorne')}
       </header>
     </div>
   );
---
+++ src/i18n.js
@@ -0,0 +1,28 @@
+import i18next from "i18next";
+import {initReactI18next} from "react-i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+  .use(initReactI18next )
+  .use(I18nextBrowserLanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    resources: {
+      fr: {
+        translation: {
+          rageBorne: 'Rage Incarnée',
+          carapaceQueen: 'Reine Carapace',
+          princeOfGluttons: 'Prince des Gloutons',
+          crookedMask: 'Masque Tordu'
+        }
+      },
+      en: {
+        translation: {
+          rageBorne: 'Rageborne',
+          carapaceQueen: 'Carapace Queen',
+          princeOfGluttons: 'Prince of Gluttons',
+          crookedMask: 'Crooked Mask'
+        }
+      }
+    }
+  })
